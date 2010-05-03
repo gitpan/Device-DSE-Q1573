715ec2416c3e7efcfa19ac2337a11fbed0a65969
David Peters <davidp@cpan.org>
2010-05-03 10:54:21 +0000

    Import of DAVIDP/Device-DSE-Q1573-0.3 from CPAN.
    gitpan-cpan-distribution: Device-DSE-Q1573
    gitpan-cpan-version:      0.3
    gitpan-cpan-path:         DAVIDP/Device-DSE-Q1573-0.3.tar.gz
    gitpan-cpan-author:       DAVIDP
    gitpan-cpan-maturity:     released

--- lib/Device/DSE/Q1573.pm
+++ lib/Device/DSE/Q1573.pm
@@ -13,7 +13,7 @@ use strict;
 BEGIN {
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $PortObj);
-    $VERSION     = '0.02';
+    $VERSION     = '0.3';
     @ISA         = qw(Exporter DynaLoader);
     @EXPORT      = qw();
     @EXPORT_OK   = qw();
